c50e24f2cd361eafe86bc5c86c5e78c6946cc11a
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2026-03-17 09:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (17.47%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.c
@@ -0,0 +1,13 @@
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + max(left, right);
+}
