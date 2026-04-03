8aebed7c4d82be50e5437bbaf1c6d64a7f62d422
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2026-04-03 14:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (30.25%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void dfs(int** isConnected, int n, int city, int* visited) {
+    visited[city] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (isConnected[city][i] == 1 && !visited[i]) {
+            dfs(isConnected, n, i, visited);
+        }
+    }
+}
+
+int findCircleNum(int** isConnected, int n, int* isConnectedColSize) {
+    int visited[n];
+    
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    int provinces = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(isConnected, n, i, visited);
+            provinces++;   // new province found
+        }
+    }
+
+    return provinces;
+}
